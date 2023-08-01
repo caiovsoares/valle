6fb9e0ba9e906bf0e0b9746be0122552bfebb25a
Caio Soares <73972985+caiovsoares@users.noreply.github.com>
2023-08-01 09:08:03 -0300

    Add files via upload

---
+++ src/pages/index.tsx
@@ -0,0 +1,137 @@
+import {
+  Flex,
+  Text,
+  Input,
+  InputGroup,
+  InputLeftAddon,
+  Button,
+  InputRightElement,
+  Select,
+} from '@chakra-ui/react';
+import { DarkModeSwitch } from '../components/DarkModeSwitch';
+import { ChangeEvent, useState } from 'react';
+
+const Index = () => {
+  const [startingValue, setStartingValue] = useState<number>(0);
+  const handleStartingValueChange = (e: ChangeEvent<HTMLInputElement>) =>
+    setStartingValue(parseFloat(e.target.value));
+
+  const [monthlyValue, setMonthlyValue] = useState<number>(0);
+  const handleMonthlyValueChange = (e: ChangeEvent<HTMLInputElement>) =>
+    setMonthlyValue(parseFloat(e.target.value));
+
+  const [interest, setInterest] = useState<number>(0);
+  const handleInterestChange = (e: ChangeEvent<HTMLInputElement>) =>
+    setInterest(parseFloat(e.target.value));
+
+  const [period, setPeriod] = useState<number>(0);
+  const handlePeriodChange = (e: ChangeEvent<HTMLInputElement>) =>
+    setPeriod(parseFloat(e.target.value));
+
+  const onSubmit = () => {};
+
+  return (
+    <Flex flexDir='column' alignItems={'center'} my={20}>
+      <Text fontSize='2xl' mb={5}>
+        Simulador de Juros Compostos
+      </Text>
+      <Flex className='calculadora' flexDir='column'>
+        <Flex flexDir={['column', 'column', 'column', 'row', 'row']}>
+          <Flex
+            w='100px'
+            justifyContent={['start', 'start', 'start', 'end', 'end']}
+            mr={2}
+            alignItems='center'
+          >
+            <Text>Valor inicial</Text>
+          </Flex>
+          <InputGroup w={300} mb={2}>
+            <InputLeftAddon children='R$' />
+            <Input type='number' onChange={handleStartingValueChange} />
+          </InputGroup>
+          <Flex
+            w='100px'
+            justifyContent={['start', 'start', 'start', 'end', 'end']}
+            mr={2}
+            alignItems='center'
+          >
+            <Text>Valor mensal</Text>
+          </Flex>
+          <InputGroup w={300} mb={2}>
+            <InputLeftAddon children='R$' />
+            <Input type='number' onChange={handleMonthlyValueChange} />
+          </InputGroup>
+        </Flex>
+        <Flex flexDir={['column', 'column', 'column', 'row', 'row']}>
+          <Flex
+            w='100px'
+            justifyContent={['start', 'start', 'start', 'end', 'end']}
+            mr={2}
+            alignItems='center'
+          >
+            <Text>Taxa de juros</Text>
+          </Flex>
+          <InputGroup w={300} mb={2}>
+            <InputLeftAddon children='%' />
+            <InputRightElement w='30'>
+              <Select defaultValue={1} borderLeftRadius={0}>
+                <option value='1'>Mensal</option>
+                <option value='2'>Anual</option>
+              </Select>
+            </InputRightElement>
+            <Input type='number' onChange={handleInterestChange} />
+          </InputGroup>
+          <Flex
+            w='100px'
+            justifyContent={['start', 'start', 'start', 'end', 'end']}
+            mr={2}
+            alignItems='center'
+          >
+            <Text>Período</Text>
+          </Flex>
+          <InputGroup w={300} mb={2}>
+            <Input type='number' onChange={handlePeriodChange} />
+            <InputRightElement w='30'>
+              <Select defaultValue={1} borderLeftRadius={0}>
+                <option value='1'>Ano(s)</option>
+                <option value='2'>Mes(es)</option>
+              </Select>
+            </InputRightElement>
+          </InputGroup>
+        </Flex>
+
+        <Button variant='outline' w={20} onClick={() => alert(interest)}>
+          Calcular
+        </Button>
+      </Flex>
+      <Flex flexDir='column'>
+        <Text>Resultado</Text>
+        <Flex flexDir={['column', 'column', 'row', 'row']}>
+          <Flex flexDir='column' border='1px' borderRadius='md' p='3' w={200}>
+            <Text>Valor total final</Text>
+            <Text fontSize='2xl'>0.000,00</Text>
+          </Flex>
+          <Flex
+            flexDir='column'
+            border='1px'
+            borderRadius='md'
+            p='3'
+            my={[4, 4, 0, 0]}
+            mx={[0, 0, 4, 4]}
+            w={200}
+          >
+            <Text>Valor total investido</Text>
+            <Text fontSize='2xl'>0.000,00</Text>
+          </Flex>
+          <Flex flexDir='column' border='1px' borderRadius='md' p='3' w={200}>
+            <Text>Total em juros</Text>
+            <Text fontSize='2xl'>0.000,00</Text>
+          </Flex>
+        </Flex>
+      </Flex>
+      <DarkModeSwitch />
+    </Flex>
+  );
+};
+
+export default Index;
