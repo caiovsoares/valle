3f48198ca7ab9d9498890c5a9c6984000d7fa50f
Bruno Valle <143638430+brunomvalle@users.noreply.github.com>
2023-08-30 19:27:59 -0300

    Update financaspessoais.tsx

--- src/pages/planilhas/financaspessoais.tsx
+++ src/pages/planilhas/financaspessoais.tsx
@@ -6,14 +6,13 @@ const financasPessoais = () => {
       <Card p={3} alignItems="center" maxW="1000px">
         <Text fontSize="4xl">Planilha de Finanças Pessoais</Text>
         <Text my={4}>
-          Organizar as suas finanças as vezes pode não ser a tarefa mais fácil do mundo, mas nem tudo está perdido.
-          Eu fiz para te ajudar uma planilha com todas as funcionalidades que você precisa para descobrir onde está o erro e se livrar de vez das dívidas!
+            Organizar as suas finanças pode não ser a tarefa mais fácil do mundo, por isso eu fiz para te ajudar uma planilha completa e totalmente gratuita
+          para te ajudar nessa missão, com ela você vai poder listar todas as suas despesas em um só lugar, separá-las por cartão de crédito, definir suas próprias
+          metas de gastos e muito mais!
         </Text>
         <Text fontSize="4xl" mt={4}>
-          Já organizou suas finanças e mesmo assim não está sobrando para investir?
-        </Text>
-        <Text my={4}>
-          Não deixe de conferir este vídeo onde eu falo sobre como ganhar renda extra!
+          Já organizou suas finanças e mesmo assim não está sobrando para investir? Que tal começar a fazer mais dinheiro com renda extra?
+          Nesse vídeo eu te dou dicas de renda extra que eu mesmo já fiz e deu certo, não deixe de conferir!
         </Text>
       </Card>
     </Flex>
