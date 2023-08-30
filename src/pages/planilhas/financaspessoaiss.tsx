f9ab9a3ff1d2b848200fbed03721789c577ae0f1
Bruno Valle <143638430+brunomvalle@users.noreply.github.com>
2023-08-30 18:56:55 -0300

    Update financaspessoaiss.tsx

--- src/pages/planilhas/financaspessoaiss.tsx
+++ src/pages/planilhas/financaspessoaiss.tsx
@@ -6,39 +6,14 @@ const financasPessoais = () => {
       <Card p={3} alignItems="center" maxW="1000px">
         <Text fontSize="4xl">Planilha de Finanças Pessoais</Text>
         <Text my={4}>
-          Vamos colocar um texto muito legal aqui... Vamos colocar um texto
-          muito legal aqui... Vamos colocar um texto muito legal aqui... Vamos
-          colocar um texto muito legal aqui... Vamos colocar um texto muito
-          legal aqui... Vamos colocar um texto muito legal aqui... Vamos colocar
-          um texto muito legal aqui... Vamos colocar um texto muito legal
-          aqui... Vamos colocar um texto muito legal aqui... Vamos colocar um
-          texto muito legal aqui... Vamos colocar um texto muito legal aqui...
-          Vamos colocar um texto muito legal aqui... Vamos colocar um texto
-          muito legal aqui... Vamos colocar um texto muito legal aqui... Vamos
-          colocar um texto muito legal aqui... Vamos colocar um texto muito
-          legal aqui... Vamos colocar um texto muito legal aqui... Vamos colocar
-          um texto muito legal aqui... Vamos colocar um texto muito legal
-          aqui... Vamos colocar um texto muito legal aqui... Vamos colocar um
-          texto muito legal aqui... Vamos colocar um texto muito legal aqui...
+          Organizar as suas finanças as vezes pode não ser a tarefa mais fácil do mundo, mas nem tudo está perdido.
+          Eu fiz para te ajudar uma planilha com todas as funcionalidades que você precisa para descobrir onde está o erro e se livrar de vez das dívidas!
         </Text>
         <Text fontSize="4xl" mt={4}>
-          O que é juros compostos
+          Já organizou suas finanças e mesmo assim não está sobrando para investir?
         </Text>
         <Text my={4}>
-          Vamos colocar um texto muito legal aqui... Vamos colocar um texto
-          muito legal aqui... Vamos colocar um texto muito legal aqui... Vamos
-          colocar um texto muito legal aqui... Vamos colocar um texto muito
-          legal aqui... Vamos colocar um texto muito legal aqui... Vamos colocar
-          um texto muito legal aqui... Vamos colocar um texto muito legal
-          aqui... Vamos colocar um texto muito legal aqui... Vamos colocar um
-          texto muito legal aqui... Vamos colocar um texto muito legal aqui...
-          Vamos colocar um texto muito legal aqui... Vamos colocar um texto
-          muito legal aqui... Vamos colocar um texto muito legal aqui... Vamos
-          colocar um texto muito legal aqui... Vamos colocar um texto muito
-          legal aqui... Vamos colocar um texto muito legal aqui... Vamos colocar
-          um texto muito legal aqui... Vamos colocar um texto muito legal
-          aqui... Vamos colocar um texto muito legal aqui... Vamos colocar um
-          texto muito legal aqui... Vamos colocar um texto muito legal aqui...
+          Não deixe de conferir este vídeo onde eu falo sobre como ganhar renda extra!
         </Text>
       </Card>
     </Flex>
