9438284dced93ea89a8cb2f5d765233420022c13
Bruno Valle <143638430+brunomvalle@users.noreply.github.com>
2023-08-30 13:55:46 -0300

    .

--- src/pages/investimentosInteligentes.tsx
+++ src/pages/investimentosInteligentes.tsx
@@ -21,15 +21,6 @@ const investimentosInteligentes = () => {
           aqui... Vamos colocar um texto muito legal aqui... Vamos colocar um
           texto muito legal aqui... Vamos colocar um texto muito legal aqui...
         </Text>
-        <iframe
-          width="304"
-          height="171"
-          src="https://www.youtube.com/embed/rq8ZUdRVDAM"
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
-        ></iframe>
         <Text fontSize="4xl" mt={4}>
           O que é juros compostos
         </Text>
