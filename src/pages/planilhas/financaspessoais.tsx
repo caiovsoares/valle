4def38f3f8247c89b831631f9b7a797fc184df59
Bruno Valle <143638430+brunomvalle@users.noreply.github.com>
2023-08-31 14:43:29 -0300

    Update financaspessoais.tsx
    TESTE 6 YT

--- src/pages/planilhas/financaspessoais.tsx
+++ src/pages/planilhas/financaspessoais.tsx
@@ -10,7 +10,7 @@ const financasPessoais = () => {
           para te ajudar nessa missão, com ela você vai poder listar todas as suas despesas em um só lugar, separá-las por cartão de crédito, definir suas próprias
           metas de gastos e muito mais!
         </Text>
-        <Youtube link="qkolQyUi_2Q" />
+        <Youtube link="qkolQyUi2Q" />
         <Text fontSize="4xl" mt={4}>
           Já organizou suas finanças e mesmo assim não está sobrando para investir?
         </Text>
