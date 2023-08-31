01d100d9240abb79cecaadd818ab1cee29f5e005
Caio Soares <73972985+caiovsoares@users.noreply.github.com>
2023-08-31 16:02:32 +0000

    Update 6 files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,14 @@ import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../theme";
 import { AppProps } from "next/app";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <Header/>
       <Component {...pageProps} />
+      <Footer/>
     </ChakraProvider>
   );
 }
