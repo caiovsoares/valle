0d2f6bff26b0de65776855cbdd11554b72e754c3
Bruno Valle <143638430+brunomvalle@users.noreply.github.com>
2023-08-31 14:30:50 -0300

    Update financaspessoais.tsx
    TESTE 3 YT

--- src/pages/planilhas/financaspessoais.tsx
+++ src/pages/planilhas/financaspessoais.tsx
@@ -10,10 +10,10 @@ const financasPessoais = () => {
           para te ajudar nessa missão, com ela você vai poder listar todas as suas despesas em um só lugar, separá-las por cartão de crédito, definir suas próprias
           metas de gastos e muito mais!
         </Text>
-        <iframe
+       <iframe
         width="100%"
         height="100%"
-        src={`https://www.youtube.com/embed/${www.youtube.com/embed/qkolQyUi_2Q?si=k6qqnLvQhPJEhOd8}`}
+        src={`https://www.youtube.com/embed/${https://www.youtube.com/watch?v=qkolQyUi_2Q}`}
         title="YouTube video player"
       ></iframe>
         <Text fontSize="4xl" mt={4}>
