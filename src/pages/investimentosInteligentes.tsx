dbbbc3ef83eba806251e8b11119bdedb26e877e7
Bruno Valle <143638430+brunomvalle@users.noreply.github.com>
2023-09-01 10:09:40 -0300

    Update investimentosInteligentes.tsx
    Formatar texto

--- src/pages/investimentosInteligentes.tsx
+++ src/pages/investimentosInteligentes.tsx
@@ -6,25 +6,26 @@ const investimentosInteligentes = () => {
       <Card p={3} alignItems="center" maxW="1000px">
         <Text fontSize="4xl">O que é Investimento?</Text>
         <Text my={4}>
-              Segundo Gustavo Cerbasi, Investir é MULTIPLICAR.
-            Esta é somente uma das muitas lições que você vai adquirir lendo o livro "Investimentos Inteligentes", que na minha opnião, é o melhor livro
-          para quem está começando ou quer começar a investir. Não, eu não estou exagerando, este livro pode ser o melhor investimento da sua vida! O autor
-          te passa conhecimento para que você multiplique seu patrimônio com as opções de investimentos disponíveis no mercado, desde renda fixa até ações e 
-          previdência privada, tudo isso usando conceitos que qualquer pessoa leiga no assunto consiga entender. Veja só alguns dos tópicos do seu livro.
-          
+              Segundo Gustavo Cerbasi, Investir é <Text fontSize="4xl">MULTIPLICAR.</Text>
+          <Text my={4}>Esta é somente uma das muitas lições que você vai adquirir lendo o livro "Investimentos Inteligentes", que na minha opnião, é o melhor livro
+          para quem está começando ou quer começar a investir.</Text>
+          <Text my={4}>
+          Não, eu não estou exagerando, este livro pode ser o melhor investimento da sua vida! O autor te passa conhecimento para que você multiplique 
+          seu patrimônio com as opções de investimentos disponíveis no mercado, desde renda fixa até ações e previdência privada, tudo isso 
+          usando conceitos que qualquer pessoa leiga no assunto consiga entender. Veja só alguns dos tópicos do seu livro.</Text>
+          <Text my={4}>
           1. Investir é multiplicar;
           2. Obstáculos ao investidor iniciante;
           3. O que não fazer;
           6. Estratégias inteligêntes em renda fixa;
           7. Estratégia inteligentes com ações;
           10. Estratégia inteligentes com imóveis.
-
+</Text>
+          <Text my={4}>
           E no meio do caminho você ainda aprende sobre SELIC, IPC-A, CDB, Clubes de Investimentos, entre outras siglas ou girias utilizadas no mercado financeiro.
           E ai, está esperando o que para aprender tudo isso?
-          
-          https://amzn.to/47WzcI8
-          
-        </Text>
+          </Text>
+          <Text my={4}>https://amzn.to/47WzcI8</Text>
       </Card>
     </Flex>
   );
