94104aca980829a9923db97479a808e85261fb4f
Bruno Valle <143638430+brunomvalle@users.noreply.github.com>
2023-09-04 11:45:47 -0300

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,15 +5,7 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html>
-        <Head /><!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-23TV0M5WJ4"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-23TV0M5WJ4');
-</script>
+        <Head />
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
           <ColorModeScript />
