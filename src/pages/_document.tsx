54a953f6da7276e4c7b7a1ec094ad586e8d4f5da
Bruno Valle <143638430+brunomvalle@users.noreply.github.com>
2023-09-04 11:44:47 -0300

    Update _document.tsx
    GOOGLE ANALYTICS

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,15 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html>
-        <Head />
+        <Head /><!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-23TV0M5WJ4"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-23TV0M5WJ4');
+</script>
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
           <ColorModeScript />
